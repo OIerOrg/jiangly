0a985d708da524519fe8db8fe3286dd4d3864f6b
Zhong Jia Xuan <134782384+zjx-kimi@users.noreply.github.com>
2024-12-03 19:31:14 +0800

    Add files via upload

---
+++ Fenwick.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using i64 = long long;
+
+template <typename T>
+struct Fenwick {
+    const int n;
+    std::vector<T> a;
+    Fenwick(int n) : n(n), a(n) {}
+    void add(int x, T v) {
+        for (int i = x + 1; i <= n; i += i & -i) {
+            a[i - 1] += v;
+        }
+    }
+    T sum(int x) {
+        T ans = 0;
+        for (int i = x; i > 0; i -= i & -i) {
+            ans += a[i - 1];
+        }
+        return ans;
+    }
+    T rangeSum(int l, int r) {
+        return sum(r) - sum(l);
+    }
+};
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    return 0;
+}
